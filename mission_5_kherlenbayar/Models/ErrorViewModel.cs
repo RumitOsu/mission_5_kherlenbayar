00178f46450b872a4eaa7b7877c373bec2cd294a
RumitOsu <103869313+RumitOsu@users.noreply.github.com>
2024-02-09 22:24:17 -0700

    Add project files.

---
+++ mission_5_kherlenbayar/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mission_5_kherlenbayar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
